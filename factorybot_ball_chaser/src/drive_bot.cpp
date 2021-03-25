f94162cf974d32d9bd760639f8c869800f3f972e
ssharma1991 <shashank.sharma.1991@outlook.com>
2021-03-25 02:07:05 -0400

    feat: added AMR ball_chasing demo

---
+++ factorybot_ball_chaser/src/drive_bot.cpp
@@ -0,0 +1,37 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "factorybot_ball_chaser/DriveToTarget.h"
+
+// ROS::Publisher motor commands;
+ros::Publisher motor_command_publisher;
+
+// TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
+// This function should publish the requested linear x and angular velocities to the robot wheel joints
+// After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
+bool handle_drive_request(factorybot_ball_chaser::DriveToTarget::Request &req, factorybot_ball_chaser::DriveToTarget::Response &res)
+{
+    geometry_msgs::Twist msg;
+    msg.linear.x=req.linear_x;
+    msg.angular.z=req.angular_z;
+    motor_command_publisher.publish(msg);
+    res.msg_feedback="Velocity request received -- Linear_x:"+std::to_string(msg.linear.x)+" and Angular_z:"+std::to_string(msg.angular.z);
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "drive_bot");
+    ros::NodeHandle n;
+
+    // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
+    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/AMR/cmd_vel", 10);
+
+    // Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
+    ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
+    ROS_INFO("Ready to send joint commands");
+
+    // Handle ROS communication events
+    ros::spin();
+
+    return 0;
+}
---
+++ factorybot_ball_chaser/src/process_image.cpp
@@ -0,0 +1,78 @@
+#include "ros/ros.h"
+#include "factorybot_ball_chaser/DriveToTarget.h"
+#include <sensor_msgs/Image.h>
+
+// Define a global client that can request services
+ros::ServiceClient client;
+
+// This function calls the command_robot service to drive the robot in the specified direction
+void drive_robot(float lin_x, float ang_z)
+{
+    factorybot_ball_chaser::DriveToTarget srv;
+    srv.request.linear_x=lin_x;
+    srv.request.angular_z=ang_z;
+    if (!client.call(srv))
+        ROS_ERROR("Failed to call service /ball_chaser/command_robot");
+}
+
+// This callback function continuously executes and reads the image data
+void process_image_callback(const sensor_msgs::Image img)
+{
+    int white_pixel = 255;
+    bool found_ball = false;
+    int leftmost_white_px = 9999;
+    int rightmost_white_px = 0;
+    for (int i = 0; i < img.height * img.step; i+=3){
+        int r = img.data[i];
+        int g = img.data[i+1];
+        int b = img.data[i+2];
+
+        if (r==white_pixel && g==white_pixel && b==white_pixel){
+            int px_col= i % img.step/3;
+            if (px_col < leftmost_white_px)
+                leftmost_white_px = px_col;
+            if (px_col > rightmost_white_px)
+                rightmost_white_px = px_col;
+            found_ball = true;
+        }
+    }
+    
+    if (!found_ball){
+        std::cout<<"Ball NOT located"<<std::endl;
+        drive_robot(0, 0);
+        return;
+    }                        
+    
+    int col_middle = (leftmost_white_px + rightmost_white_px)/2;
+    if (col_middle < img.width * 0.4) {
+        std::cout<<"Ball located on LEFT"<<std::endl;
+        drive_robot(0.3, 1);
+    } else if (col_middle > img.width * 0.6) {
+        std::cout<<"Ball located on RIGHT"<<std::endl;
+        drive_robot(0.3, -1);
+    } else if (leftmost_white_px < (img.width * 0.4) && rightmost_white_px > (img.width * 0.6)) {
+        std::cout<<"Ball located too CLOSE"<<std::endl;
+        drive_robot(0, 0);
+    } else {
+        std::cout<<"Ball located at CENTER"<<std::endl;
+        drive_robot(0.3, 0);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    // Initialize the process_image node and create a handle to it
+    ros::init(argc, argv, "process_image");
+    ros::NodeHandle n;
+
+    // Define a client service capable of requesting services from command_robot
+    client = n.serviceClient<factorybot_ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
+
+    // Subscribe to /camera/rgb/image_raw topic to read the image data inside the process_image_callback function
+    ros::Subscriber sub1 = n.subscribe("/AMR/camera/rgb/image_raw", 10, process_image_callback);
+
+    // Handle ROS communication events
+    ros::spin();
+
+    return 0;
+}
